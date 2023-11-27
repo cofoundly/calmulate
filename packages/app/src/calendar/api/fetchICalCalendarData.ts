6385ec65c0cb7b2b7eb19017bcba8a7b1dc35728
Shaun Saker <sakershaun@gmail.com>
2023-11-27 14:59:23 +1000

    feat(app): temporarily disable live data for demo

--- packages/app/src/calendar/api/fetchICalCalendarData.ts
+++ packages/app/src/calendar/api/fetchICalCalendarData.ts
@@ -1,16 +1,19 @@
 import { mockICalData } from '../fixtures/mockICalData'
 
 export const fetchICalCalendarData = async (url: string) => {
+  console.log(url)
+
   // in development, we can't fetch the calendar data from the server because of CORS
-  if (import.meta.env.DEV) {
-    // set a timeout to simulate a slow network
-    await new Promise(resolve => setTimeout(resolve, 1000))
+  // TODO: SS temporarily disabled for demo
+  // if (import.meta.env.DEV) {
+  // set a timeout to simulate a slow network
+  await new Promise(resolve => setTimeout(resolve, 1000))
 
-    return mockICalData
-  }
+  return mockICalData
+  // }
 
-  const response = await fetch(url)
-  const data = await response.text()
+  // const response = await fetch(url)
+  // const data = await response.text()
 
-  return data
+  // return data
 }
