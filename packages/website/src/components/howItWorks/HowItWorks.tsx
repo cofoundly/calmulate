1cdc9c815456e618018ee708b0156e64c15ca896
Shaun Saker <sakershaun@gmail.com>
2023-11-27 10:56:36 +1000

    fix(website): HowItWorks section on mobile

--- packages/website/src/components/howItWorks/HowItWorks.tsx
+++ packages/website/src/components/howItWorks/HowItWorks.tsx
@@ -16,26 +16,24 @@ export const HowItWorks = ({ steps, ...sectionProps }: Props) => {
   return (
     <Section {...sectionProps}>
       <Container className="mt-12 lg:mt-24">
-        <ul className="gap-6 flex items-center flex-wrap sm:flex-nowrap">
-          {steps?.length
-            ? steps.map((step, index) => (
-                <Fragment key={step.title}>
-                  <li className="flex-1 flex flex-col items-center gap-2 text-center relative pt-16">
-                    <div className="opacity-30 absolute inset-0 text-9xl font-bold">{index + 1}</div>
+        <ul className="gap-6 flex items-center flex-col lg:flex-row">
+          {steps?.map((step, index) => (
+            <Fragment key={step.title}>
+              <li className="flex-1 flex flex-col items-center gap-2 text-center relative pt-16">
+                <div className="opacity-30 absolute inset-0 text-9xl font-bold">{index + 1}</div>
 
-                    <TitleText>{step.title}</TitleText>
+                <TitleText>{step.title}</TitleText>
 
-                    <ParagraphText>{step.description}</ParagraphText>
-                  </li>
+                <ParagraphText>{step.description}</ParagraphText>
+              </li>
 
-                  {index !== steps.length - 1 ? (
-                    <div>
-                      <ArrowRightIcon className="w-10 h-10" />
-                    </div>
-                  ) : null}
-                </Fragment>
-              ))
-            : null}
+              {index !== steps?.length - 1 ? (
+                <div>
+                  <ArrowRightIcon className="w-10 h-10 rotate-90 lg:rotate-0" />
+                </div>
+              ) : null}
+            </Fragment>
+          ))}
         </ul>
       </Container>
     </Section>
